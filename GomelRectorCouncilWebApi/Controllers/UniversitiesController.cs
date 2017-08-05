3a7fc2782880fb76f6b3f76598c4287e2997d711
Oleg <olgasn@inbox.ru>
2017-08-05 17:46:42 +0300

    Операции в контроллере

--- GomelRectorCouncilWebApi/Controllers/UniversitiesController.cs
+++ GomelRectorCouncilWebApi/Controllers/UniversitiesController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using GomelRectorCouncilWebApi.Models;
 using GomelRectorCouncilWebApi.Data;
@@ -27,27 +25,58 @@ namespace GomelRectorCouncilWebApi.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int UniversityId)
         {
-            return "value";
+            University university = _context.Universities.FirstOrDefault(x => x.UniversityId == UniversityId);
+            if (university == null)
+                return NotFound();
+            return new ObjectResult(university);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]University university)
         {
+            if (university == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Universities.Add(university);
+            _context.SaveChanges();
+            return Ok(university);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]University university)
         {
+            if (university == null)
+            {
+                return BadRequest();
+            }
+            if (!_context.Universities.Any(x => x.UniversityId == university.UniversityId))
+            {
+                return NotFound();
+            }
+
+            _context.Update(university);
+            _context.SaveChanges();
+            return Ok(university);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int universityId)
         {
+            University university = _context.Universities.FirstOrDefault(x => x.UniversityId == universityId);
+            if (university == null)
+            {
+                return NotFound();
+            }
+            _context.Universities.Remove(university);
+            _context.SaveChanges();
+            return Ok(university);
         }
     }
 }
